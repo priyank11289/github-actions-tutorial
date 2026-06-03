721b4c17f0b44aa4a7e673dda3e2a81846eaf373
Priyank <priyankshri125@email.com>
2026-06-03 13:11:57 +0900

    semicolon deleted

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main() {
     Calculator calc;
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "2 + 3 = " << calc.add(2, 3) << std::endl
+    std::cout << "2 + 3 = " << calc.add(2, 3) << std::endl;
+    
     return 0;
 }
