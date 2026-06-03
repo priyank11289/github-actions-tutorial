86b45b2ac2d437f1bece4b2e8f3c7e845c743586
Priyank <priyankshri125@email.com>
2026-06-03 12:04:12 +0900

    Add calculator feature

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "Calculator.h"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    Calculator calc;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "2 + 3 = " << calc.add(2, 3) << std::endl;
+    return 0;
 }
