b8554a619cc6d5dcd5e5db42395edf67b5182bd6
Priyank <priyankshri125@email.com>
2026-06-03 13:14:53 +0900

    add tests and linters

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include "Calculator.h"
+#include <iostream>
+
+int main() {
+    Calculator calc;
+    // We expect 2 + 3 to equal 5
+    if (calc.add(2, 3) != 5) {
+        std::cerr << "Test failed! 2+3 did not equal 5." << std::endl;
+        return 1; // Returning a non-zero number tells GitHub the test failed!
+    }
+    std::cout << "Test passed!" << std::endl;
+    return 0;
+}
